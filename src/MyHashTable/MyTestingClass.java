328cb5771ff8dd5734b37b89c439991a6f7045a4
Saulet <231003@astanait.edu.kz>
2024-05-15 20:45:22 +0500

    2st Milestone

--- src/MyHashTable/MyTestingClass.java
+++ src/MyHashTable/MyTestingClass.java
@@ -1,6 +1,39 @@
 package MyHashTable;
 
 public class MyTestingClass {
+    private int id;
+    private String name;
 
+    public MyTestingClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters and setters for id and name (if needed)
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyTestingClass that = (MyTestingClass) obj;
+        return id == that.id && name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MyTestingClass{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
+
 
